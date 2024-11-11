0c77665c1f3dfbcf1ccd7e04c158e0bdd9532d42
Luan <luuanreis@gmail.com>
2024-11-11 05:21:53 -0300

    created button component

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,9 @@
+import { Container } from './styles';
+
+export function Button() {
+  return (
+    <Container type="button">
+      <h1>Adicionar Filme</h1>
+    </Container>
+  );
+}
--- src/styles/global.js
+++ src/styles/global.js
@@ -10,6 +10,13 @@ export default createGlobalStyle`
   body {
     background-color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
     color: ${({ theme }) => theme.COLORS.WHITE};
+
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button, textarea {
+    font-family: "Roboto Slab", serif;
+    font-size: 16px;
   }
 
   a {
