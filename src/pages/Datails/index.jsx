0fff296e91f7b2dcd1286807acf0ceb7e621e20b
Luan <luuanreis@gmail.com>
2024-11-14 04:55:39 -0300

    created header

--- src/pages/Datails/index.jsx
+++ src/pages/Datails/index.jsx
@@ -1,14 +1,15 @@
 import { Container } from './styles';
 
+import { Header } from '../../components/Header';
 import { Button } from '../../components/Button';
 
 export function Details(){
 
   return(
     <Container>
-      <h1>Hello World</h1>
+      <Header/>
 
-      <Button title="Login" loading />
+      <Button title="Voltar" /> 
     </Container>
   )
 
